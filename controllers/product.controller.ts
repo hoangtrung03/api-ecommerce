bf56cbacd31f21c16e145817011870da8ed2daf9
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2023-03-22 11:00:13 +0700

    +Update: location img

--- controllers/product.controller.ts
+++ controllers/product.controller.ts
@@ -13,11 +13,14 @@ import { ORDER, SORT_BY } from '../constants/product'
 
 export const handleImageProduct = (product) => {
   if (product.image !== undefined && product.image !== '') {
-    product.image = HOST + `/${ROUTE_IMAGE}/` + product.image
+      //link img
+    product.image = product.image
   }
   if (product.images !== undefined && product.images.length !== 0) {
     product.images = product.images.map((image) => {
-      return image !== '' ? HOST + `/${ROUTE_IMAGE}/` + image : ''
+      //link img
+      // return image !== '' ? HOST + `/${ROUTE_IMAGE}/` + image : ''
+      return image !== '' ? image : ''
     })
   }
   return product
