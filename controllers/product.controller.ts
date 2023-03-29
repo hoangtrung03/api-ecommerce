8ca136160272193d6e662da5c0011341d8e98352
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2023-03-29 11:30:55 +0700

    +Update: search

--- controllers/product.controller.ts
+++ controllers/product.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { responseSuccess, ErrorHandler } from '../utils/response'
 import { ProductModel } from '../database/models/product.model'
 import { STATUS } from '../constants/status'
-import mongoose from 'mongoose'
+import mongoose, { Document } from 'mongoose'
 import { isAdmin } from '../utils/validate'
 import { uploadFile, uploadManyFile } from '../utils/upload'
 import { HOST } from '../utils/helper'
@@ -306,24 +306,61 @@ const deleteManyProducts = async (req: Request, res: Response) => {
 //   return responseSuccess(res, response)
 // }
 
-const searchProducts = async (req: Request, res: Response) => {
+// const searchProduct = async (req: Request, res: Response) => {
+//   const { searchText }: { [key: string]: string | any } = req.query;
+//   const searchExpression = { $text: { $search: searchText } };
+//   const condition = searchExpression;
+
+//   let products: any = await ProductModel.find(condition)
+//     .populate('category')
+//     .sort({ createdAt: -1 })
+//     .select('-description -__v')
+//     .lean();
+//   products = products.map((product) => handleImageProduct(product));
+
+//   const response = {
+//     message: 'Tìm các sản phẩm thành công',
+//     data: products,
+//   };
+//   return responseSuccess(res, response);
+// };
+
+const searchProduct = async (req: Request, res: Response) => {
   try {
-    const { searchText } = req.query;
-    const products = await ProductModel.find({ $text: { $search: searchText } })
+    const { searchText } = req.query
+    const products = await ProductModel.find(
+      { $text: { $search: String(searchText) } },
+      (err: any, docs: Document<Product>[]) => {
+        if (err) console.error(err)
+        const response = {
+          message: 'Tìm kiếm sản phẩm thành công',
+          data: docs,
+        }
+        return responseSuccess(res, response)
+      }
+    )
       .populate('category')
       .select('-description -__v')
       .sort({ createdAt: -1 })
-      .lean();
+      .lean()
     const response = {
       message: 'Tìm kiếm sản phẩm thành công',
       data: products,
-    };
-    return responseSuccess(res, response);
+    }
+    return responseSuccess(res, response)
   } catch (error) {
-    console.error(error);
-    return responseError(res, 500, 'Đã có lỗi xảy ra trong quá trình tìm kiếm');
+    console.error(error)
   }
-};
+}
+
+const uploadProductImage = async (req: Request, res: Response) => {
+  const path = await uploadFile(req, FOLDERS.PRODUCT)
+  const response = {
+    message: 'Upload ảnh thành công',
+    data: path,
+  }
+  return responseSuccess(res, response)
+}
 
 const uploadManyProductImages = async (req: Request, res: Response) => {
   const paths = await uploadManyFile(req, FOLDERS.PRODUCT)
